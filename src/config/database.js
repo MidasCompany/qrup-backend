402365078e49003347ade3336a8241a73b67dc7b
Alessandro Santos <alessandro.vncs@gmail.com>
2020-03-08 12:30:45 -0300

    Remoção do eslint, e bibliotecas inuteis e mudanças no travis

--- src/config/database.js
+++ src/config/database.js
@@ -11,20 +11,13 @@ module.exports = {
 	},
 	development: {
 
-		dialect: 'postgres',
-		host: '192.168.99.100',
-		username: 'postgres',
-		password: 'docker',
-		database: 'qrup',
-		dialectOptions: {
-			dateString: true,
-			typeCast: true,
-		},
+		database: 'test',
+		storage: 'test.sqlite',
+		dialect: 'sqlite',
 		define: {
 			timestamps: true,
 			underscored: true,
 			underscoredALL: true,
 		},
-		logging: false,
 	},
 };
