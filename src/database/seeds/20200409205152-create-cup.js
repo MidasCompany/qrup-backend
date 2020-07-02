d211a87f86017c00d35651416cea5974d9f2f698
Alessandro <alessandro.vncs@gmail.com>
2020-07-02 16:03:34 -0300

    Fixação do codigo do copo e script de reset de base

--- src/database/seeds/20200409205152-create-cup.js
+++ src/database/seeds/20200409205152-create-cup.js
@@ -3,27 +3,27 @@ const uuid = require('uuid');
 
 
 module.exports = {
-	up: (queryInterface, Sequelize) => {
+  up: (queryInterface, Sequelize) => {
 
-		let listCups = []
+    let listCups = []
 
-		for( let i = 0; i < 10; i++){
-			let uuid_p = uuid.v4();
-			let qr = uuid_p.split('-')[0];
-			
-			listCups.push({
-				id: uuid_p, 
-				qr,
-				type: '400ml', 
-				created_at: new Date(), 
-				updated_at: new Date(),
-			})
-		}
+    for (let i = 0; i < 10; i++) {
+      let uuid_p = uuid.v4();
+      let qr = uuid_p.split('-')[0];
 
-		return queryInterface.bulkInsert('cups', listCups, {});
-	},
+      listCups.push({
+        id: uuid_p,
+        qr: 'ffa7fcfe',
+        type: '400ml',
+        created_at: new Date(),
+        updated_at: new Date(),
+      })
+    }
 
-	down: (queryInterface, Sequelize) => {
-		return queryInterface.bulkDelete('cups', null, {});
-	}
+    return queryInterface.bulkInsert('cups', listCups, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('cups', null, {});
+  }
 };
