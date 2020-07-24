7ca254f6bf584bf070a6d0d5065b9d9632e808d8
Alessandro Santos <alessandro.vncs@gmail.com>
2020-07-23 21:35:39 -0300

    Script de test

--- src/config/database.js
+++ src/config/database.js
@@ -10,6 +10,16 @@ module.exports = {
       underscoredALL: true
     }
   },
+  test: {
+    database: 'test',
+    storage: 'test.sqlite',
+    dialect: 'sqlite',
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredALL: true
+    }
+  },
   development: {
     database: 'test',
     storage: 'test.sqlite',
