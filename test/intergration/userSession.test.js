f7ff7fd5f12d6cf32b7464ecae3225b7c60ccff6
Alessandro Santos <alessandro.vncs@gmail.com>
2020-07-31 21:06:50 -0300

    Correçoes nos testes

--- test/intergration/userSession.test.js
+++ test/intergration/userSession.test.js
@@ -11,6 +11,21 @@ describe('Authentication', () => { // categorizar os testes
   beforeAll(() => {
   })
 
+  it('should create company', async () => {
+    const response = await request(app)
+      .post('/companies')
+      .send({
+        nameCompany: 'Midas',
+        cnpj: '13086620000137',
+        address: 'Alcindo Cacela',
+        contact: '212121210',
+        nameOwner: 'Amaury Pinto',
+        cpf: '33670919015',
+        password: 'midas'
+      })
+    expect(response.status).toBe(200)
+  })
+
   it('should create user', async () => {
     const user = await request(app)
       .post('/users')
